1d037e710bb4f4069c640ac0636e84ae64f6a42f
Daniel Józefiuk <daniel.jozefiuk@bcoders.pl>
2021-09-08 12:51:18 +0200

    SkillRequest added.

---
+++ CoCAppAPI/Library/DbLibrary/Models/Skill/Request/SkillRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DbLibrary.Models.Skill.Request
+{
+    public class SkillRequest
+    {
+        public Guid CharacterId { get; set; }
+        public Guid DefaultSkillId { get; set; }
+        public int Value { get; set; }
+        public int Advancement { get; set; }
+    }
+}
